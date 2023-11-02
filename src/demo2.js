dcff9e421ba2b30b619f9f49be348eda3a02162e
MeghaGajera2908 <meghagajera.dreamspot@gmail.com>
2023-11-02 18:08:43 +0530

    change h2

--- src/demo2.js
+++ src/demo2.js
@@ -4,6 +4,7 @@ function Myfunctions() {
   return (
     <div>
       <img className='dining' alt='dinning' src='https://sayajihotels.com/images/hotels/sayaji-rajkot/overview/dining.jpg' />
+      <h2>Test</h2>
     </div>
   );
 }
